ac596d35313597d011b26c4e4fd6dcbcbd4a4c18
Selvavignesh Gurunathan Rajeshwari <selvamarshall13@gmail.com>
2025-07-13 19:02:14 +0530

    Time: 1 ms (59.84%) | Memory: 44.8 MB (60.70%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,8 +3,9 @@ class Solution {
         int j=0;
         for(int i=1; i<nums.length; i++){
             if(nums[i] != nums[j]){
+                
+                nums[j+1] = nums[i];
                 j++;
-                nums[j] = nums[i];
             }
         }
         return j+1;
