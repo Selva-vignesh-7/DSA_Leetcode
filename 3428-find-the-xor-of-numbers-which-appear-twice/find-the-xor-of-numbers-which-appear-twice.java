c9a9332f4fab28dd1a88d920e7cc4f2d4334a2db
Selvavignesh Gurunathan Rajeshwari <selvamarshall13@gmail.com>
2025-07-18 14:04:29 +0530

    Time: 1 ms (98.58%) | Memory: 43.1 MB (64.62%) - LeetSync

--- 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
@@ -1,21 +1,29 @@
 class Solution {
     public int duplicateNumbersXOR(int[] nums) {
         int xor=0;
-        int maxi=0;
-        //Arrays.sort(nums);
-        for(int i=0; i<nums.length; i++){
-            maxi = Math.max(maxi, nums[i]);
-        }
-        int hash[] = new int[maxi+1];
-        //int hash[] = new int[51] // for limited constraints
-        for(int i=0; i<nums.length; i++){
+        // int maxi=0;
+        // Arrays.sort(nums);
+        // for(int i=0; i<nums.length; i++){
+        //     maxi = Math.max(maxi, nums[i]);
+        // }
+        // int hash[] = new int[maxi+1];
+        // //int hash[] = new int[51] // for limited constraints
+        // for(int i=0; i<nums.length; i++){
             
-            hash[nums[i]]++;
+        //     hash[nums[i]]++;
         
-            if(hash[nums[i]] > 1){
-                xor = xor ^ nums[i];
-                hash[nums[i]]--;
+        //     if(hash[nums[i]] > 1){
+        //         xor = xor ^ nums[i];
+        //         hash[nums[i]]--;
+        //     }
+        // }
+
+        boolean[] seen = new boolean[51];
+        for(int num : nums){
+            if(seen[num]){
+                xor = xor ^ num;
             }
+            seen[num] = true;
         }
         return xor;
     }
