38b5695cb2d6f063f8a386d821a1e7d41b8a45f8
Selvavignesh Gurunathan Rajeshwari <selvamarshall13@gmail.com>
2025-07-29 11:40:01 +0530

    Time: 1916 ms (9.02%) | Memory: 44.7 MB (49.47%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,16 +1,15 @@
 class Solution {
     public String longestPalindrome(String s) {
-        char[] ch = s.toCharArray();
-        int currLength = 0, maxLength = 0;
+
+        int currLength = 0, maxLength = Integer.MIN_VALUE, len = s.length();
         String maxString = "";
-        for (int i = 0; i < ch.length; i++) {
-            for (int j = i; j < ch.length; j++) {
+        for (int i = 0; i < len ; i++) {
+            for (int j = i; j < len; j++) {
                 if (checkPalindrome(s, i, j)) {
                     currLength = j - i + 1;
                     if (maxLength < currLength) {
                         maxLength = currLength;
                         maxString = s.substring(i, j + 1);
-                        System.out.println(maxString);
                     }
 
                 }
@@ -22,7 +21,7 @@ class Solution {
     public boolean checkPalindrome(String s, int i, int j) {
         int start = i;
         int end = j;
-        while (start <= end && end >= 0 && start <= s.length() - 1) {
+        while (start <= end && end >= 0 && start < s.length()) {
             if (s.charAt(start) != s.charAt(end)) {
                 return false;
             }
@@ -30,5 +29,12 @@ class Solution {
             end--;
         }
         return true;
+
+        // String str = s.substring(i, j + 1);
+        // StringBuilder sb = new StringBuilder(str);
+        // String revString = sb.reverse().toString();
+        // if(revString.equals(str)) return true;
+        // return false;
+        // gives TLE because of creating string builder object using forloop multiple times
     }
 }
